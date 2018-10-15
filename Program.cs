480324c163306390e2d5685095a8a921ce8cc300
mateus <mateusvanhalen@gmail.com>
2018-10-15 10:23:28 -0500

    mon

--- Program.cs
+++ Program.cs
@@ -3,10 +3,62 @@ using System.Collections.Generic;
 using System.Linq;
 namespace linq
 {
-    class Program
+    public class Program
     {
         public string fruits {get; set;}
-        static void Main()
+        public List<Bank> Banks { get; set;}
+        public List<Bank> banks { get; set; }
+
+        public class Bank
+
+            {
+            public string Symbol { get; set; }
+            public string Name { get; set; }
+            }
+
+        public class Customer
+            {
+            public string Name { get; set; }
+            public double Balance { get; set; }
+            public string Bank { get; set; }
+            }
+
+        public static void Main() {
+
+            List<Bank> banks = new List<Bank>() {
+            new Bank(){ Name="First Tennessee", Symbol="FTB"},
+            new Bank(){ Name="Wells Fargo", Symbol="WF"},
+            new Bank(){ Name="Bank of America", Symbol="BOA"},
+            new Bank(){ Name="Citibank", Symbol="CITI"},
+
+            };
+
+             List<Customer> customers = new List<Customer>() {
+            new Customer(){ Name="Bob Lesman", Balance=80345.66, Bank="FTB"},
+            new Customer(){ Name="Joe Landy", Balance=9284756.21, Bank="WF"},
+            new Customer(){ Name="Meg Ford", Balance=487233.01, Bank="BOA"},
+            new Customer(){ Name="Peg Vale", Balance=7001449.92, Bank="BOA"},
+            new Customer(){ Name="Mike Johnson", Balance=790872.12, Bank="WF"},
+            new Customer(){ Name="Les Paul", Balance=8374892.54, Bank="WF"},
+            new Customer(){ Name="Sid Crosby", Balance=957436.39, Bank="FTB"},
+            new Customer(){ Name="Sarah Ng", Balance=56562389.85, Bank="FTB"},
+            new Customer(){ Name="Tina Fey", Balance=1000000.00, Bank="CITI"},
+            new Customer(){ Name="Sid Brown", Balance=49582.68, Bank="CITI"}
+        };
+
+            // List<ReportItem> millionaireReport = new List<ReportItem>() {
+            //         from m in Customer.Balance
+            //         where m > 10000000
+            //         select m;
+
+            // Console.WriteLine("Here is a list of millionaires");
+            //     foreach (var m in millionaireReport)
+            //     {
+            //         Console.WriteLine(m);
+            //     }
+
+
+
         {
 
             List<string> fruits = new List<string>() {"Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry"};
@@ -69,4 +121,5 @@ namespace linq
 
         }
     }
+}
 }
